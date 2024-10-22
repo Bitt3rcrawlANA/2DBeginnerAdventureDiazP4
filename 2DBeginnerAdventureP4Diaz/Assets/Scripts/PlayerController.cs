b6b338a62ab44595d591fe8b42e97e3cf8e7bced
Bitt3rcrawlANA <722697@student.dusd.net>
2024-10-22 11:45:42 -0700

    update progress
    player now can move and tiles were created

--- 2DBeginnerAdventureP4Diaz/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Diaz/Assets/Scripts/PlayerController.cs
@@ -8,39 +8,21 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Debug.Log(horizontal);
-
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
         Debug.Log(vertical);
 
-
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.y = position.y + 1f * vertical;
+        position.x = position.x + 1f * horizontal;
         transform.position = position;
     }
 }
