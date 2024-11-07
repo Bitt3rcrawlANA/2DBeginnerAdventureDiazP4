8d86d24e0cb05d85f0a05dc66cd21a018c11d88b
Bitt3rcrawlANA <722697@student.dusd.net>
2024-11-07 11:41:47 -0800

    animations
    starts ruby animations

--- 2DBeginnerAdventureP4Diaz/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Diaz/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
-{ 
-    // Variables related to temporary invincibility
-    public float timeInvincible = 2.0f;
-    bool isInvincible;
-    float damageCooldown;
-
-    public float speed = 7.0f;
-    public int maxHealth = 5;
-    public int health { get { return currentHealth; } }
-    int currentHealth;
+{
+    public float speed = 3.0f;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+        
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float damageCooldown;
+
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,54 +33,63 @@ public class PlayerController : MonoBehaviour
 
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
-        Debug.Log(vertical);
+        vertical = Input.GetAxis("Verrtical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if (isInvincible)
         {
             damageCooldown -= Time.deltaTime;
             if (damageCooldown < 0)
-            {
                 isInvincible = false;
-            }
-        }
 
+        }
     }
+
+    // FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x
-                     + 6f
-                     * horizontal
-                     * Time.deltaTime;
-        position.y = position.y
-            + 6f
-            * vertical
-            * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
-
     }
+
+
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
-            {
                 return;
-            }
+
             isInvincible = true;
             damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
         }
 
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
     }
+
 }
